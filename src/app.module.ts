98624ebf6626f14676a8d62ab129bb0c4f83cc8a
ManuelR <mnlrpr@gmail.com>
2025-02-26 11:48:42 -0500

    Config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'sports_space',
+      entities: [],
+      synchronize: false, //Evitar cambiar la database con las class entities
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
